1c7d5d699c3a773ec272ff8e5f2c179772a18a88
Nikita Gureev <gureev6856@gmail.com>
2023-03-11 18:21:57 +0300

    new: support video, get split/merge audio and video

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,12 @@
 import os
 
 import telebot
+from moviepy.audio.AudioClip import CompositeAudioClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+from convert import convert_video_to_audio_moviepy
+
 
 # root_path = '/home/nikita/Sound/bot'
 
@@ -19,15 +25,17 @@ def bot_init():
     def chat(message):
         if message.text == 'Привет':
             bot.send_message(message.from_user.id,
-                             'Отправь аудио или голосовуху боту. Он почистит твой голос от мата и паразитных слов.')
+                             'Отправь аудио, видео или голосовое боту. Он почистит твой голос от мата и паразитных слов.')
 
-    @bot.message_handler(content_types=['audio', 'voice'])
+    @bot.message_handler(content_types=['audio', 'voice', 'video'])
     def file_process(message):
         # receive
         if message.content_type == 'audio':
             file_info = bot.get_file(message.audio.file_id)
         if message.content_type == 'voice':
             file_info = bot.get_file(message.voice.file_id)
+        if message.content_type == 'video':
+            file_info = bot.get_file(message.video.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
         storage_file_path = prepare_storage_path(file_info.file_path)
         with open(storage_file_path, 'wb') as new_file:
@@ -35,12 +43,33 @@ def bot_init():
         print('Download completed')
         bot.send_message(message.from_user.id, 'Аудио получено. Подожди немного...')
 
+        if message.content_type == 'video':
+            storage_video_file_path = storage_file_path
+            storage_file_path = convert_video_to_audio_moviepy(storage_file_path)
+
+        # -----------------
+        # process code
+        # -----------------
+
+        # -----------------
+        # pics audio
+        # -----------------
+
+        if message.content_type == 'video':
+            video_clip = VideoFileClip(storage_video_file_path)
+            audio_clip = AudioFileClip(storage_file_path)
+            final_clip = video_clip.set_audio(audio_clip)
+            storage_file_path = prepare_storage_path("final_" + storage_video_file_path[storage_video_file_path.find('/') + 1:])
+            final_clip.write_videofile(storage_file_path)
+
         # send
         with open(storage_file_path, 'rb') as file:
             if message.content_type == 'audio':
                 bot.send_audio(message.from_user.id, audio=file)
             if message.content_type == 'voice':
                 bot.send_voice(message.from_user.id, voice=file)
+            if message.content_type == 'video':
+                bot.send_video(message.from_user.id, video=file)
 
         print('Send completed')
 
