f7fc08ed26610af04aa32fd6771dc772e2c74d8a
Nikita Gureev <gureev6856@gmail.com>
2023-03-11 17:10:54 +0300

    fix: fatal bug when storage not exists

--- bot/bot.py
+++ bot/bot.py
@@ -2,12 +2,18 @@ import os
 
 import telebot
 
+# root_path = '/home/nikita/Sound/bot'
+
+def prepare_storage_path(raw_path: str):
+    path = raw_path[raw_path.find('/'):]
+    print(path)
+    return './storage' + path
+
 
 def bot_init():
     # создаем объект бота
     bot = telebot.TeleBot(os.environ.get("TG_BOT_TOKEN"))
-
-    # обработчик любых сообщений
+    os.makedirs(os.path.dirname('storage/'), exist_ok=True)
 
     @bot.message_handler(content_types=['text'])
     def chat(message):
@@ -17,17 +23,20 @@ def bot_init():
 
     @bot.message_handler(content_types=['audio', 'voice'])
     def file_process(message):
-        bot.send_message(message.from_user.id, 'Аудио получено. Подожди немного...')
+        # receive
         if message.content_type == 'audio':
             file_info = bot.get_file(message.audio.file_id)
         if message.content_type == 'voice':
             file_info = bot.get_file(message.voice.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        with open('storage/' + file_info.file_path, 'wb') as new_file:
+        storage_file_path = prepare_storage_path(file_info.file_path)
+        with open(storage_file_path, 'wb') as new_file:
             new_file.write(downloaded_file)
         print('Download completed')
+        bot.send_message(message.from_user.id, 'Аудио получено. Подожди немного...')
 
-        with open('storage/' + file_info.file_path, 'rb') as file:
+        # send
+        with open(storage_file_path, 'rb') as file:
             if message.content_type == 'audio':
                 bot.send_audio(message.from_user.id, audio=file)
             if message.content_type == 'voice':
@@ -35,5 +44,4 @@ def bot_init():
 
         print('Send completed')
 
-    # запускаем бота
     bot.polling()
