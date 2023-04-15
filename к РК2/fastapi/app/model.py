44d7f16207f27da596979b54f6c3445e5ffe7715
kaizer-nurik <kerimovnm@mail.ru>
2023-04-15 10:37:56 +0300

    whisperX

--- к РК2/fastapi/app/model.py
+++ к РК2/fastapi/app/model.py
@@ -1,13 +1,14 @@
 from df.enhance import enhance, init_df, load_audio, save_audio
 from df.utils import download_file
 import whisper 
-from stable_whisper import modify_model
+import whisperx
+# from stable_whisper import modify_model
 import os
 from datetime import datetime
 import multiprocessing as mp
 import json
 STORAGE = "./content"
-
+DEVICE = 'cpu'
 def add_timestamp(filename:str):
     return filename[:filename.rfind('.')] +'_'+datetime.now().strftime("%m-%d-%Y_%H.%M.%S")+filename[filename.rfind('.'):]
     
@@ -26,28 +27,36 @@ def init_storage_path():
 def get_timestamps_and_profanity(result_dict):
     ban_words = ['еб','сука','сук',"блять","бля","пидор","гей","мудак","муд","говно","говн","уебище","уёби","еба","ёба", "жопа", "еблан", "долбаёб","далбаёб","долбаеб","далбаеб","жопа","зад","дроч","ху","ублюдок","пиздец","пизда","пиз","еба"]
 
-
+    
     words = []
-    for segment in result_dict['segments']:
-        for word in segment['words']:
-            is_profanity = False
-            if word['word'].lower() in ban_words:
-                is_profanity = True
-            else:
-                for ban in ban_words:
-                    if ban in word['word'].lower():
-                        is_profanity = True
-                        break
-            word['is_profanity'] = is_profanity
-            words.append(word)
+    
+    for word in result_dict:
+        is_profanity = False
+        if word['text'].lower() in ban_words:
+            is_profanity = True
+        else:
+            for ban in ban_words:
+                if ban in word['text'].lower():
+                    is_profanity = True
+                    break
+        temp_word = {
+            'word':word['text'],
+            'start':word['start'],
+            'end':word['end'],
+            'is_profanity':is_profanity,
+            'tokens':[1],
+            'probability':1.0                
+        }
+        words.append(temp_word)
+
     return words
 
 
 
 def process_init():
     DFmodel, df_state, _ = init_df(post_filter = False,config_allow_defaults = True)
-    whisper_model = whisper.load_model('small')
-    modify_model(whisper_model)
+    whisper_model = whisper.load_model('small',device = DEVICE)
+    #modify_model(whisper_model)
     return whisper_model, DFmodel, df_state
 
 def proccess_audio(audio_path,whisper_model,deepfilter_model,df_state):
@@ -59,10 +68,19 @@ def proccess_audio(audio_path,whisper_model,deepfilter_model,df_state):
     enhanced_audio_path = audio_path[:audio_path.rfind('.')]+"_enhanced.wav"
     save_audio(enhanced_audio_path,enhanced, df_state.sr())
     
-    result = whisper_model.transcribe(enhanced_audio_path, language='ru', suppress_silence=True, ts_num=16)
-    result_dict = result.to_dict() 
-    text = result_dict["text"]
-    words = get_timestamps_and_profanity(result_dict)
+
+
+    
+    result = whisper_model.transcribe(enhanced_audio_path, language='ru')
+
+    model_a, metadata = whisperx.load_align_model(language_code='ru', device=DEVICE)
+
+    # align whisper output
+    result_aligned = whisperx.align(result["segments"], model_a, metadata, enhanced_audio_path, DEVICE)
+
+    # result_dict = result.to_dict() 
+    text = result["text"]
+    words = get_timestamps_and_profanity(result_aligned['word_segments'])
     
     return enhanced_audio_path, text, words
 
@@ -70,25 +88,25 @@ def worker_ant(queue:mp.Queue, err_queue:mp.Queue):
     whisper_model, DFmodel, df_state = process_init()
     while True:
         audiofile_path = queue.get()
-        try:
+        # try:
             
             
-            open(audiofile_path+'.blocked','wb')
-            enhanced_audio_path, text, words =proccess_audio(audiofile_path,whisper_model, DFmodel, df_state)  
-            response_json ={
-                "text":text,
-                "words":words
-            }
-            with open(audiofile_path+'.json', 'w', encoding='utf8') as outfile:
-                json.dump(response_json, outfile, ensure_ascii=False)
-            os.remove(audiofile_path+'.blocked')
-        except BaseException as e:
-            print(e)
-            err_queue.put(audiofile_path, 0)
+        open(audiofile_path+'.blocked','wb')
+        enhanced_audio_path, text, words =proccess_audio(audiofile_path,whisper_model, DFmodel, df_state)  
+        response_json ={
+            "text":text,
+            "words":words
+        }
+        with open(audiofile_path+'.json', 'w', encoding='utf8') as outfile:
+            json.dump(response_json, outfile, ensure_ascii=False)
+        os.remove(audiofile_path+'.blocked')
+        # except BaseException as e:
+        #     print(e)
+        #     err_queue.put(audiofile_path, 0)
             
-            with open(audiofile_path+'.error','w') as f:
-                f.write(repr(e))
-            os.remove(audiofile_path+'.blocked')
+        #     with open(audiofile_path+'.error','w') as f:
+        #         f.write(repr(e))
+        #     os.remove(audiofile_path+'.blocked')
 
             
 
