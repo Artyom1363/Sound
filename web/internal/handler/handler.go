e12acee011992bf6ea0e1eaf190d854eee75cdec
Никита Гуреев <--->
2023-05-07 19:30:58 +0300

    update: remove limit

--- web/internal/handler/handler.go
+++ web/internal/handler/handler.go
@@ -67,14 +67,14 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte(fmt.Sprintf("Неверный формат файла. Загружайте только mp3!")))
 			return
 		}
-		if meta.Length() > MaxAudioDuration {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(fmt.Sprintf("Лимит продолжительности аудио: 30 минут. "+
-				"Продолжительность загруженного файла: %f секунд", meta.Length().Seconds(),
-			)))
-			return
-		}
-		log.Printf("Audio duration: %f sec.", meta.Length().Seconds())
+		//if meta.Length() > MaxAudioDuration {
+		//	w.WriteHeader(http.StatusBadRequest)
+		//	w.Write([]byte(fmt.Sprintf("Лимит продолжительности аудио: 30 минут. "+
+		//		"Продолжительность загруженного файла: %f секунд", meta.Length().Seconds(),
+		//	)))
+		//	return
+		//}
+		log.Printf("Audio duration: %f minutes", meta.Length().Minutes())
 
 		w.Write([]byte("/fileserver/" + handler.Filename))
 	}
